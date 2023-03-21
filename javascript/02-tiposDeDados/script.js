4aeb16167290e9322c2c4faf27016989be130bb3
João Giovani <giovannirs957@gmail.com>
2023-03-21 20:45:17 -0300

    Tipos de dados

---
+++ javascript/02-tiposDeDados/script.js
@@ -0,0 +1,37 @@
+var nome = 'André';
+var idade = 22;
+var time = null;
+var simbolo = Symbol();
+
+
+
+console.log(typeof simbolo);
+
+
+var nome = 'João';
+var sobrenome = 'Freire';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto)
+
+
+
+var nome = 'Pelé';
+var gols = '1080';
+var totalGolsPele = nome + ' ' + gols;
+console.log(totalGolsPele)
+
+console.log('ola meu "nome" e joao')
+
+
+
+var jogador = 'Romário';
+
+var gols = 1000;
+
+var frase2 = `Romário fez ${gols + 700} gols`;
+
+console.log(frase2);
+
+
+
---
+++ javascript/02-tiposDeDados/scriptAula.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+var nome = 'João'
+// Declare uma variável contendo um número dentro de uma string
+var idade = '23'
+console.log(typeof idade);
+
+// Declare uma variável com a sua idade
+var minhaIdade = 22;
+console.log(minhaIdade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var myName = 'João';
+var mySobrenome = 'Giovani Freire';
+var completeName = myName + ' ' +  mySobrenome;
+console.log(completeName)
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var time = "It's time";
+console.log(time);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof myName);
