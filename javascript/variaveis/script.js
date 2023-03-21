3b00874336c5462868226612339181c443e61a69
João Giovani <giovannirs957@gmail.com>
2023-03-21 19:53:11 -0300

    variáveis

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,41 @@
+// Variaveis = var let e const
+var nome = 'joao';
+var idade = 23;
+var possuiFaculdade = true;
+
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael', 
+    cidade = 'rio';
+
+
+console.log(sobrenome, cidade);
+
+
+var precoAplicativo;
+
+console.log(precoAplicativo)
+
+var nome = 'João';
+var idade = 22;
+var comidaFavorita;
+
+comidaFavorita = 'Lasanha';
+
+var estado,
+     pais,
+      regiao,
+       telefone,
+        capital;
+
+
+
+
+
