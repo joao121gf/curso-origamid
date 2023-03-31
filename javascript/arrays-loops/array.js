7662a9d4d89c20a0f5a163fef7d762a00ed2efd9
João Giovani <giovannirs957@gmail.com>
2023-03-31 18:22:00 -0300

    Loops e arrays

---
+++ javascript/arrays-loops/array.js
@@ -0,0 +1,13 @@
+var videoGames = ["Switch", "PS4", "XBox"];
+
+console.log(videoGames);
+console.log(videoGames[1])
+
+
+
+var ultimoItem = videoGames.pop();
+
+console.log(videoGames)
+console.log(ultimoItem)
+
+
---
+++ javascript/arrays-loops/loop.js
@@ -0,0 +1,15 @@
+for (let c = 1; c <= 10; c++) {
+  console.log(c);
+}
+
+// Inicio condicao incremento
+// o numero é 1
+// enquanto ele for menor que 10
+// faça isso
+
+// while
+var i = 10;
+while (i > 0) {
+  console.log(i);
+  i--;
+}
