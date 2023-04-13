f7a2cc180e1f96568e4c9d2953f90faccd219c91
João Giovani <giovannirs957@gmail.com>
2023-04-13 20:03:53 -0300

    navegacao por tabs

--- javascript/traversing-e-manipulacao/exercicio.js
+++ javascript/traversing-e-manipulacao/exercicio.js
@@ -1,30 +1,25 @@
 // Duplique o menu e adicione ele em copy
 
-const menu = document.querySelector('.menu')
+const menu = document.querySelector(".menu");
 
-const copy = document.querySelector('.copy')
+const copy = document.querySelector(".copy");
 
-menu.cloneNode(true)
-copy.appendChild(menu)
-
-
+menu.cloneNode(true);
+copy.appendChild(menu);
 
 // Selecione o primeiro DT da dl de Faq
-const primeiraDl = document.querySelector('dl')
-const primeiroDt = primeiraDl.children[0]
-console.log(primeiroDt)
-
+const primeiraDl = document.querySelector("dl");
+const primeiroDt = primeiraDl.children[0];
+console.log(primeiroDt);
 
 // Selecione o DD referente ao primeiro DT
 
-const primeiroDD = primeiroDt.nextElementSibling
+const primeiroDD = primeiroDt.nextElementSibling;
 
-console.log(primeiroDD)
-
+console.log(primeiroDD);
 
 // Substitua o conteúdo html de .faq pelo de .animais
-const animais = document.querySelector('.animais')
-const faq = document.querySelector('.faq')
+const animais = document.querySelector(".animais");
+const faq = document.querySelector(".faq");
 
-
-faq.innerHTML = animais.innerHTML
+faq.innerHTML = animais.innerHTML;
