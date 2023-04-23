b0dfcf47e9fe06d23ca5e72ad03d91ab495df288
João Giovani <giovannirs957@gmail.com>
2023-04-23 12:52:44 -0300

    Mais exercícios de arrays

--- javascript/exercícios próprios/array/script.js
+++ javascript/exercícios próprios/array/script.js
@@ -7,6 +7,14 @@
 
 // d) Crie uma função que recebe um array como parâmetro e retorna um novo array com os valores duplicados removidos.
 
+// e) Crie uma função que recebe um array como parâmetro e retorna o primeiro elemento do array.
+
+// f) Crie uma função que recebe um array como parâmetro e retorna o último elemento do array.
+
+// g) Crie uma função que recebe um array como parâmetro e retorna o número de elementos presentes no array.
+
+// h) Crie uma função que recebe um array como parâmetro e retorna um novo array com os elementos em ordem crescente.
+
 // a
 function receiveArray(arrayParameter){
     return Math.max(...arrayParameter)
@@ -27,7 +35,6 @@ const numbersArray = [5, 2, 6, 10, 2, 4, 10]
 let itensArray =  somaArray(numbersArray)
 console.log(`The sum of values in array is ${itensArray}`)
 
-
 // c
 
 function concatArray(array1, array2){
@@ -39,7 +46,6 @@ let array1 = ['This array', 'Togheter with']
 let array2 = ['This :)']
 concatArray(array1, array2)
 
-
 // d
 function cleanArrays(value){
     return value.filter((item, index) =>
@@ -48,6 +54,39 @@ function cleanArrays(value){
 }
 let notes = [1, 5, 6, 2, 5, 23, 5, 4, 2]
 cleanArrays(notes)
-console.log(`A array sem propriedades repetidas fica ${cleanArrays(notes)}`)
+console.log(`The array without repeating properties becomes ${cleanArrays(notes)}`)
 
+// e
+function firstElementArray(value){
+    return value[0]
+}
 
+const eArray = ['pedro', 'josé', 'beatriz', 'nicolly']
+const firstElement = firstElementArray(eArray)
+
+console.log(`The first element of array is ${firstElement}`)
+
+// f
+function lastElement(value){
+    // return value.length(-1)
+    return (value[value.length - 1])
+}
+const fArray = ['zacarias', 'pele', 'garrincha', 'sócrates', 'bebeto']
+const fLastElement = lastElement(fArray)
+console.log(`The last elemento of Array is ${fLastElement}`)
+
+// g
+
+function gArray(item){
+    return item.length
+}
+const gArray3 = [2, 5, 1, 5, 6, 3]
+const gLastElement = gArray(gArray3)
+console.log(gLastElement)
+
+// f
+function fArray2(item){
+    return item.sort()
+}
+const fArrayy = [9, 8, 7, 5, 4 ,3, 5]
+console.log(fArray2 (fArrayy))
