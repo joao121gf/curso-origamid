b27c86efad9971a60b4478cb4f6a33c901618a7e
João Giovani <giovannirs957@gmail.com>
2023-05-07 14:42:40 -0300

    Dropdown menu para mobiles

--- javascript/dropwdown/js/modules/dropdown.js
+++ javascript/dropwdown/js/modules/dropdown.js
@@ -1,22 +1,20 @@
+import outsideClick from "./outsideClick.js";
+
 export default function dropdownMenu() {
-  
-  }
+  window.addEventListener("load", () => {
+    const sobre2 = document.querySelectorAll("[data-dropdown]");
+    sobre2.forEach((menu) => {
+      ["touchstart", "click"].forEach((userEvent) => {
+        menu.addEventListener(userEvent, ativaSobre);
+      });
+    });
 
-
-
-window.addEventListener("load", () => {
-  const sobre = document.querySelector('[data-dropdown]');
-  
-  function ativaSobre(event){
-    event.preventDefault()
-    this.classList.toggle('ativaDropdown')
-    console.log(this)
-  }
-  sobre.addEventListener('click', ativaSobre)
-  sobre.addEventListener('touchstart', ativaSobre)
-
-  function mouseForaSobre(event){
-    this.classList.remove('ativaDropdown')
-  }
-  sobre.addEventListener('mouseleave', mouseForaSobre)
-})
+    function ativaSobre(event) {
+      event.preventDefault();
+      this.classList.add("ativaDropdown");
+      outsideClick(this, ["touchstart", "click"], () => {
+        this.classList.remove("ativaDropdown");
+      });
+    }
+  });
+}
