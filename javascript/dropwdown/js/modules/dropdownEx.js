e98c91be563d0a4be009700173dd0c53f5cbdffd
João Giovani <giovannirs957@gmail.com>
2023-05-08 22:08:48 -0300

    Criando funcao outsideclick

--- javascript/dropwdown/js/modules/dropdownEx.js
+++ javascript/dropwdown/js/modules/dropdownEx.js
@@ -1,6 +1,31 @@
 
-export default function dropdownMenuEx() {
+export default function dropdownMenuEx(element, event, callback) {
   window.addEventListener("load", () => {
-    
+    const contato = document.querySelectorAll('[data-contact]');
+
+
+    function outClick(callback){
+      const html = document.documentElement;
+      html.addEventListener('click', handleOut);
+      function handleOut(event){
+        callback();
+      }
+    }
+  
+
+
+    contato.forEach((items) =>{
+      items.addEventListener('click', mostraTab);
+    })
+    function mostraTab(event, target){
+      event.preventDefault();
+      this.classList.add('desceTab')
+      // console.log(event.target)
+      outClick(()=> {
+        console.log(element)
+      })
+
+      // se eu clicar fora remove o desce tab
+    }
   });
 }
